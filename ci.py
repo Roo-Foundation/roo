fdd1c0313c7878f572c9be1644dc61b33845eefa
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-02-08 20:28:14 -0800

    Fix CI and added print

--- ci.py
+++ ci.py
@@ -4,10 +4,11 @@ t = """
 # roo
 Images of roo
 """
-for roo in os.listdir("./roos"):
-    roo = roo.split(".")[0]
-    t += f'\n\n\n### {roo}\n<img src="https://raw.githubusercontent.com/Roo-Foundation/roo/main/roos/{roo}.png" width="130" height="130">'
 
+for roo in os.listdir("./roos"):
+    roo_name = roo.split(".")[0]
+    t += f'\n\n\n### {roo_name}\n<img src="https://raw.githubusercontent.com/Roo-Foundation/roo/main/roos/{roo}" width="130" height="130">'
+    print(f"Updated {roo}")
 
 with open("README.md", "w") as f:
     f.write(t)
